bdb5840796b3d3f355950b1ef49d792a0cbfe8f1
joaovictorferian <joaovictorferian@gmail.com>
2025-10-02 12:44:55 -0300

    Backend: Definição de rotas e conexão com o DB

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.use((req, res, next) => {
+    ("Requisição:", req.method, JSON.stringify(req.url));
+    next();
+  });
+
+const PORT = process.env.PORT || 500;
+app.listen(PORT, () => {
+  console.log(`Server rodando na porta ${PORT}`);
+});
